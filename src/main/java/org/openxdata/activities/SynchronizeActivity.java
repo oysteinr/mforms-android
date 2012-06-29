e72eedd33aceaa7b8fb1f9e9cbf29d94f97cede5
Øystein Lund Rolland <oysteinrolland@gmail.com>
2012-06-29 15:28:15 +0200

    Managed to request and retrieve studies through a custom built proto

--- src/main/java/org/openxdata/activities/SynchronizeActivity.java
+++ src/main/java/org/openxdata/activities/SynchronizeActivity.java
@@ -1,44 +1,48 @@
 package org.openxdata.activities;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.http.client.ClientProtocolException;
 import org.openxdata.R;
+import org.openxdata.mforms.model.StudyDef;
 import org.openxdata.transport.HttpTransport;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 
 public class SynchronizeActivity extends Activity {
+
+	List<StudyDef> studies;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sync);
 
-		downloadUsers();
-
+		studies = downloadStudies();
 	}
 
-	private Object[] downloadUsers() {
+	private List<StudyDef> downloadStudies() {
 
-		Object[] data = null;
+		List<StudyDef> data = null;
 
-		ProgressDialog progressDialog = ProgressDialog.show(this, "Synchronizing", "Fetching Data...");
-		Log.i("Synchronize Activity", "Downloading Users");
+		Log.i("Synchronize Activity", "Downloading Studies");
 		HttpTransport transport = new HttpTransport("http://10.0.2.2:8889/mpsubmit");
 
 		try {
 			data = transport.getData();
 		} catch (ClientProtocolException e) {
-			e.printStackTrace();
+			Log.i("Synchronize Activity", "ClientProtocolException: " + e.getMessage());
 		} catch (IOException e) {
-			e.printStackTrace();
+			Log.i("Synchronize Activity", "IOException: " + e.getMessage());
+		} catch (InstantiationException e) {
+			Log.i("Synchronize Activity", "InstantiationException: " + e.getMessage());
+		} catch (IllegalAccessException e) {
+			Log.i("Synchronize Activity", "IllegalAccessException: " + e.getMessage());
 		}
 
-		progressDialog.dismiss();
 		return data;
 	}
 }
--- src/main/java/org/openxdata/transport/HttpTransport.java
+++ src/main/java/org/openxdata/transport/HttpTransport.java
@@ -6,13 +6,19 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.client.ClientProtocolException;
+import org.openxdata.mforms.model.FormDef;
 import org.openxdata.mforms.model.RequestHeader;
+import org.openxdata.mforms.model.ResponseHeader;
 import org.openxdata.mforms.model.StudyDef;
-import org.openxdata.mforms.model.StudyDefList;
+import org.openxdata.mforms.persistent.PersistentHelper;
 
 import android.util.Log;
+
+import com.jcraft.jzlib.ZInputStream;
 
 public class HttpTransport {
 
@@ -22,11 +28,21 @@ public class HttpTransport {
 		this.serverURL = url;
 	}
 
-	public Object[] getData() throws ClientProtocolException, IOException {
+	@SuppressWarnings("static-access")
+	public List<StudyDef> getData() throws ClientProtocolException, IOException, InstantiationException,
+			IllegalAccessException {
 
 		Log.i("HttpTransport", "Fetching Data");
 
-		URL url = new URL(serverURL + "?action=" + RequestHeader.ACTION_DOWNLOAD_STUDIES_FORMS);
+		List<StudyDef> studies = new ArrayList<StudyDef>();
+		RequestHeader requestHeader = new RequestHeader();
+		requestHeader.setLocale("en");
+		requestHeader.setAction(RequestHeader.ACTION_DOWNLOAD_STUDY_LIST);
+		requestHeader.setSerializer("android-proto-1.0-SNAPSHOT");
+		requestHeader.setUserName("admin");
+		requestHeader.setPassword("admin");
+
+		URL url = new URL(serverURL + "?action=" + RequestHeader.ACTION_DOWNLOAD_STUDY_LIST);
 
 		URLConnection urlConn = url.openConnection();
 		HttpURLConnection httpConn = (HttpURLConnection) urlConn;
@@ -38,46 +54,52 @@ public class HttpTransport {
 		httpConn.setDoInput(true);
 		httpConn.setDoOutput(true);
 
+		DataInputStream in = null;
 		DataOutputStream out = new DataOutputStream(httpConn.getOutputStream());
 
-		int response = 0;
-
 		try {
-			out.writeUTF("admin");
-			out.writeUTF("admin");
-			out.writeUTF("mforms-proto-2.1");
-			// out.writeUTF(RequestHeader.getSerializer());
-			out.writeUTF(Byte.toString(RequestHeader.ACTION_DOWNLOAD_STUDIES_FORMS));
-			out.writeUTF("en");
 
-			out.flush();
-			out.close();
-
-			response = httpConn.getResponseCode();
+			requestHeader.write(out);
+			int response = httpConn.getResponseCode();
 			Log.i("Responsecode:", "" + response);
+			in = new DataInputStream(httpConn.getInputStream());
+			if (response == HttpURLConnection.HTTP_OK) {
+
+				ZInputStream zis = new ZInputStream(in);
+				DataInputStream zdis = new DataInputStream(zis);
+
+				ResponseHeader responseHeader = new ResponseHeader();
+				
+				responseHeader.read(zdis);
+				byte responseStatusCode = ((ResponseHeader) responseHeader).getStatus();
+				if (responseStatusCode == ResponseHeader.STATUS_SUCCESS) {
+
+					int numberOfStudies = zdis.readShort();
+
+					for (int i = 0; i < numberOfStudies; i++) {
+
+						StudyDef study = new StudyDef();
+
+						zdis.readInt();
+
+						study.setId(zdis.readInt());
+						study.setName(zdis.readUTF());
+						study.setVariableName(zdis.readUTF());
+						study.setForms(PersistentHelper.readMedium(zdis, FormDef.class));
+						
+						studies.add(study);
+					}
+				}
+			}
 
 		} catch (IOException e) {
 			Log.e("HttpTransport", "writing to stream failed", e);
+		} finally {
+			out.flush();
+			out.close();
+			in.close();
 		}
 
-		DataInputStream in = new DataInputStream(httpConn.getInputStream());
-		
-		// Some magic is needed here for reading the input-stream. Investigate mforms to figure it out
-		try {
-			int studID = in.readInt();
-			Log.i("studID: ", "" + studID);
-		} catch (IOException e) {
-			Log.e("HttpTransport", "Unable to read studID", e);
-		}
-		
-
-		in.close();
-
-		if (response == HttpURLConnection.HTTP_OK) {
-			Log.i("HttpTransport: ", "disconnecting!");
-			httpConn.disconnect();
-		}
-
-		return null;
+		return studies;
 	}
 }
